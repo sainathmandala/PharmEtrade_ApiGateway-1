83a14962bfa52e878c80e5956607e91888cff68f
DELL <DELL@DESKTOP-PN1GOC1>
2024-07-01 14:56:08 +0530

    test1

--- PharmEtrade_ApiGateway/Program.cs
+++ PharmEtrade_ApiGateway/Program.cs
@@ -6,7 +6,7 @@ using PharmEtrade_ApiGateway.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container juuv.
 ///
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
