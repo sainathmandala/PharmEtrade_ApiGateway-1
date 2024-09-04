f209ab4b72a647ae1ec6722300f3717958c6b8b7
Surendra Babu V <surendrababuvemuri@gmail.com>
2024-09-04 12:00:56 +0530

    Added Payment Model

---
+++ BAL/Models/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Models
+{
+    public class Payment
+    {
+        public string? PaymentId {get;set;}
+        public string OrderId { get;set;}
+        public int PaymentMethodId { get;set;}
+        public int PaymentStatusId { get;set;}
+        public decimal Amount { get;set;}
+        public DateTime PaymentDate { get;set;}
+    }
+}
